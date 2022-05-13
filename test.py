22f79dcb31559a579bb7f24c0ed15bdca6fef68c
Hyonchori <gus8cjf@naver.com>
2022-05-13 18:38:12 +0900

    test file

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+import sys
 import torch
+from torch.utils.data.dataloader import DataLoader
 
 from Data.for_train.coco_dataset import COCODataset
 from Data.for_train.augmented_dataset import AugmentedDataset
@@ -8,24 +10,41 @@ from Model.yolox import YOLOX
 from Model.model_utils import model_info
 
 from Train.losses.detection_loss import get_detection_losses
+from Train.train_utils import autobatch
 
 if __name__ == "__main__":
     model = YOLOX(name="yolox")
-    #model.eval()
-    model_info(model)
+    '''gpu = True
+    cuda = gpu and torch.cuda.is_available()
+    device = torch.device('cuda:0' if cuda else "cpu")
+    model = model.to(device)
+    autobatch(model)
+    sys.exit()'''
+    model.eval()
 
     val_dir = "/home/daton/Downloads/coco/val2017"
     val_path = "/home/daton/Downloads/coco/annotations_trainval2017/annotations/instances_val2017.json"
 
-    val_dir = "/media/jhc/4AD250EDD250DEAF/dataset/coco/val2017"
-    val_path = "/media/jhc/4AD250EDD250DEAF/dataset/coco/annotations_trainval2017/annotations/instances_val2017.json"
+    #val_dir = "/media/jhc/4AD250EDD250DEAF/dataset/coco/val2017"
+    #val_path = "/media/jhc/4AD250EDD250DEAF/dataset/coco/annotations_trainval2017/annotations/instances_val2017.json"
 
     img_size = (720, 1280)
     preproc = Preprocessing(img_size=img_size)
-    dataset = COCODataset(val_dir, val_path, img_size=img_size, preproc=preproc)
+    dataset = COCODataset(val_dir, val_path, ("bbox", "segmentation"), img_size=img_size, preproc=preproc)
     dataset = AugmentedDataset(dataset, img_size=img_size, preproc=preproc)
 
-    for img0, img, labels0, labels, img_info, img_id in dataset:
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=2,
+        shuffle=True,
+        collate_fn=dataset.collate_fn
+    )
+
+    for img0, img, labels0, labels, img_shape, img_id in dataloader:
+        print(img.shape)
+        break
+
+    '''for img0, img, labels0, labels, img_info, img_id in dataset:
         print(img.shape)
         img = torch.from_numpy(img)[None]
         print(img.shape)
@@ -43,6 +62,6 @@ if __name__ == "__main__":
                 origin_preds,
                 dtype
             )
-        plot_labels(img0, labels0)
+        plot_labels(img0, labels0)'''
 
 
